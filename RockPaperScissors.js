581611a11bf6ffd6e394fa5654637d3d77bca366
mdu74 <mdu74.C@gmail.com>
2017-09-07 10:19:56 +0200

    Spacing
    Cleaned up the code. (Just added spacing)

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,22 +1,23 @@
 function RockPaperScissors(choose, opponent) {
     var opponentResult = getResultFromOpponent(opponent);
-    if (opponentResult == "Scissors" && choose == "Rock")
+    if (opponentResult === "Scissors" && choose === "Rock")
         return "Win";
 	
-    if (opponentResult == "Rock" && choose == "Scissors")
+    if (opponentResult === "Rock" && choose === "Scissors")
         return "Loose";
 	
-    if (opponentResult == "Paper" && choose == "Scissors")
+    if (opponentResult === "Paper" && choose === "Scissors")
         return "Win";
 	
-    if (opponentResult == "Scissors" && choose == "Paper")
+    if (opponentResult === "Scissors" && choose === "Paper")
         return "Loose";
 	
-    if (opponentResult == "Rock" && choose == "Paper")
+    if (opponentResult === "Rock" && choose === "Paper")
         return "Win";
 	
-    if (opponentResult == "Paper" && choose == "Rock")
+    if (opponentResult === "Paper" && choose === "Rock")
         return "Loose";
+    
     return "Draw";
 }
 
@@ -31,6 +32,7 @@ function RandomResultOpponent() {
     function getRandomResult() {
         var opponentOptions = ["Rock", "Paper", "Scissors"];
         var randomResult = opponentOptions[Math.floor(Math.random() * opponentOptions.length)];
+        
         return randomResult;
     }
 }
